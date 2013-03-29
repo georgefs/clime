09fd782e81c96f44768313a73fba1ccc60445086
george <goblin_george@gmail.com>
2013-03-29 14:56:45 +0800

    return error code = 1 if cli program throw exception

--- clime/core.py
+++ clime/core.py
@@ -557,10 +557,10 @@ class Program(object):
             if self.debug:
                 from traceback import print_exception
                 print_exception(*sys.exc_info())
-                return
+                exit(1)
             else:
                 self.complain(e)
-                return
+                exit(1)
 
         if not self.ignore_return and return_val is not None:
             if inspect.isgenerator(return_val):
